886590553121ee38b709217672347145e92a60af
Rebecca Chen <rchen@college.harvard.edu>
2013-04-21 13:38:43 -0400

    pushing again, something weird happened. rchen

--- FinalProject/src/image_comparer/PixelArray.java
+++ FinalProject/src/image_comparer/PixelArray.java
@@ -41,14 +41,35 @@ public class PixelArray
 	
 	public PixelArray round(int colors)
 	{
+		int colorUnit = 256/colors;
+		
 		int w = getWidth();
 		int h = getHeight();
 		int[][] roundedArray = new int[w][h];
 		for (int i = 0; i < w; i++)
 			for (int j = 0; j < w; j++)
 			{
+				int pixel = getPixel(i, j);
+				int redBelow = (getRed(pixel)/colorUnit) * colorUnit;
+				int greenBelow = (getGreen(pixel)/colorUnit) * colorUnit;
+				int blueBelow = (getBlue(pixel)/colorUnit) * colorUnit;
 				
+				double minDistance = Double.MAX_VALUE;
+				for (int k = 0; k < 8; k++)
+				{
+					int red = redBelow + ((k >> 4) & 0x00000002);
+					int green = greenBelow + ((k >> 2) & 0x00000002);
+					int blue = blueBelow + (k & 0x00000002);
+					int roundedPixel = (red << 4) + (green << 2) + blue;
+					double distance = getDistance(pixel, roundedPixel);
+					if (distance < minDistance)
+					{
+						minDistance = distance;
+						roundedArray[i][j] = roundedPixel;
+					}
+				}
 			}
+		return new PixelArray(roundedArray);
 	}
 	
 	private double getDistance(int p1, int p2)
@@ -56,7 +77,6 @@ public class PixelArray
 		double r1 = (double) getRed(p1);
 		double r2 = (double) getRed(p2);
 		
-		double rMean = (r1 + r2) / 2.;
 		double weight = (r1 + r2) / 512. + 2;
 		return Math.sqrt(weight * Math.pow(r1-r2, 2) + 4*Math.pow((double) getGreen(p1) - (double) getGreen(p2), 2) + weight * Math.pow((double) getBlue(p1) - (double) getBlue(p2), 2));
 	}
