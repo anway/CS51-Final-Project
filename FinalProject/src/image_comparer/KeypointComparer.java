60c07ea700f5c90c80ae2ec433778f3a357a1ca9
Annie Wei <anniewei@sbcglobal.net>
2013-04-21 11:31:52 -0400

    small change

--- FinalProject/src/image_comparer/KeypointComparer.java
+++ FinalProject/src/image_comparer/KeypointComparer.java
@@ -11,7 +11,7 @@ public class KeypointComparer implements PixelArrayComparer
 	public boolean contains(int[] array1, int[] array2, int value1, int value2){
 		int length = array1.length; //they're the same length
 		for(int i=0; i<length; i++){
-			if (value1 == array1[i]) && (value2 == array2[i])
+			if (value1 == array1[i] && value2 == array2[i])
 				return true;
 		}
 		return false;
@@ -32,17 +32,17 @@ public class KeypointComparer implements PixelArrayComparer
 		int height = a1.getHeight();
 		// since there aren't any tuples we'll just use two arrays
 		// to record points already picked
-		int[n] pickedWidth;
-		int[n] pickedHeight;
-		int numMatched, currWidth, currHeight;
+		int[] pickedWidth;
+		int[] pickedHeight;
+		int numMatched=0, currWidth, currHeight;
 		Random rand = new Random();
 		for (int i=0; i<n; i++){
 			do {
 				currWidth = rand.nextInt(width);
 				currHeight = rand.nextInt(height);
 			}
-			while (contains(pickedWidth, currWidth, pickedHeight, currHeight));
-			if (a1.getPixel[currWidth][currHeight]==a2.getPixel[currWidth][currHeight])
+			while (contains(pickedWidth, pickedHeight, currWidth, currHeight));
+			if (a1.getPixel(currWidth, currHeight)==a2.getPixel(currWidth, currHeight))
 				numMatched++;
 			pickedWidth[n]=currWidth;
 			pickedHeight[n]=currHeight;
